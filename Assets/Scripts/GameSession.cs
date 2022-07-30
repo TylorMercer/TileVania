287152af1a31c53de5488c60c72252bdcfbd3f70
HelexBlade <tylor.mercer@hotmail.com>
2022-07-30 11:54:27 -0230

    Adding scoreboard
    Keeps track of lives and score when picking up coins.

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -3,10 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playersLives = 3;
+    [SerializeField] int score = 0;
+
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
     void Awake()
     {
         int numGameSeesions = FindObjectsOfType<GameSession>().Length;
@@ -30,11 +35,24 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    void Start() 
+    {
+        livesText.text = playersLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
+    }
+
     void TakeLife()
     {
         playersLives--;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playersLives.ToString();
     }
 
     void ResetGameSession()
